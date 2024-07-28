2e314bd3b8283b1a3760e47b861271d288b74d03
djima <daiki.applecreate@gmail.com>
2024-07-28 10:12:48 +0900

    サンプルコードを追加して実機確認済み

--- ScrollList-Basic/app/src/main/java/com/example/scrolllist_basic/MainActivity.kt
+++ ScrollList-Basic/app/src/main/java/com/example/scrolllist_basic/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,8 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ScrollListBasicTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ScrollList(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +32,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScrollList(modifier: Modifier = Modifier) {
+
+    //  テストデータの生成
+    val testData = List(100) { "データ項目 ${it + 1}" }
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        //  1つのデータを表示
+        item {
+            Text(text = "First Item")
+        }
+
+        //  複数のデータを表示
+        items(testData) { data ->
+            Text(text = data)
+        }
+
+        //  1つのデータを表示
+        item {
+            Text(text = "Last Item")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ScrollListBasicTheme {
-        Greeting("Android")
+        ScrollList()
     }
 }
